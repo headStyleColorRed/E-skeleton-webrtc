e99cac69da4b31d90e96a7b67e8169d5eb4e1e7f
Rodrigo Labrador <headstylecolorred@gmail.com>
2021-01-30 21:10:19 +0100

    pipeline not coming

--- server/app.js
+++ server/app.js
@@ -1,43 +1,48 @@
-
 const express = require("express")
 const app = express();
-const server = require("http").Server(app)
-const io = require("socket.io")(server)
-const bodyParser = require("body-parser")
-const Cors = require("cors")
-const { v4: uuidV4 } = require("uuid")
+var server = require('http').createServer(app);
+var io = require('socket.io')(server, { origins: '*:*' });
 const port = 8893;
+const { v4: uuidV4 } = require("uuid")
+const Cors = require("cors")
 
-// Middlewares
+
+
 app.use(Cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
 
-// Open port
-server.listen(port, () => console.log("Listening port " + port));
-
-
-// ++++++++++++++++ HTTP METHODS +++++++++++++++++++ //
-
-app.get("/", (req, res) => {
-	res.send("E-skeleton-webrtc is up and running! :D")
-})
-
-
-app.get("/join-room", (req, res) => {
-	res.redirect(`/${uuidV4()}`)
-})
-
-app.get("/:room", (req, res) => {
-	res.send("loool")
-} )
-
-app.get("/users", async (req, res) => {					//	 B O R R A R
-	const users = await User.find();					//	 B O R R A R
-	res.json(users);									//	 B O R R A R
+server.listen(port, () => {
+    console.log('server started on PORT 8893');
 });
 
-app.get("/deleteUsers", async (req, res) => {			//	 B O R R A R
-	await User.deleteMany();							//	 B O R R A R
-	res.json("Users deleted");							//	 B O R R A R
+app.get('/', (req, res) => {
+	res.send("E-skeleton-webrtc is up and running! :D")
+});
+
+
+io.on('connection', async (socket) => {
+    console.log('a user connected');
+    
+
+    socket.on("join-room", (roomId) => {
+        console.log("user joined room: " + roomId);
+        socket.join(roomId)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+	});
+	
+	socket.on('stream', (stream, id) => {
+		console.log('streamiiingj');
+		console.log(stream);
+		console.log(id);
+    });
+
+    socket.on("message", (stream, room) => {
+		console.log("streaming");
+		console.log(stream);
+        io.sockets.in(room).emit('server-stream', stream);
+    })
+
+
 });
---
+++ server/webExample/WebRtc.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="rtcWrapper">
+  </div>
+</template>
+
+<script>
+
+// Things to do in order to use this file
+// 1. Fill the publicKey
+// 2. Pass the itemId
+// 3. Total amount prop is optional
+
+let publicKey = "YOUR PUBLIC KEY"
+let stripe = Stripe(publicKey)
+let elements = stripe.elements()
+let card = undefined
+
+export default {
+  name: "WebRtc",
+  props: {
+    itemId: String,
+    totalAmount: String,
+  },
+  data() {
+    return {
+      
+    };
+  },
+  mounted() {
+    
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style >
+
+</style>
