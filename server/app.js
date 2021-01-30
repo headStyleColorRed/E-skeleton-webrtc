c53e2c2e8bb8b12adc30e8efa2088a0e0fde58dc
Rodrigo Labrador <headstylecolorred@gmail.com>
2021-01-30 17:11:22 +0100

    automatic redirecting

--- server/app.js
+++ server/app.js
@@ -1,50 +1,20 @@
 
 const express = require("express")
 const app = express();
-const puerto = parseInt(process.env.PORT, 10) || 8890;
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
 const bodyParser = require("body-parser")
 const Cors = require("cors")
-const mongoose = require("mongoose")
-const environment = process.env.NODE_ENV
-var dbLink = new String()
-
-
-// Modules
-const User = require("./mongoDB/userModel.js")
-
-
-// Set environment
-if (environment == "production")
-	dbLink = "mongodb://webrtc_DB:27017/mongowebrtc"
-else 
-	dbLink = "mongodb://localhost:27017/mongowebrtc"
-
+const { v4: uuidV4 } = require("uuid")
+const port = 8893;
 
 // Middlewares
 app.use(Cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
-// Routes
-app.use("/webrtc", require("./requests/webrtcRequests"))
-
-
 // Open port
-app.listen(puerto, () => console.log("Listening port " + puerto))
-
-
-// DataBase connection
-let timeOut = setInterval(() => {
-	mongoose.connect(dbLink, { useNewUrlParser: true }, (err) => {
-		if (err) {
-			console.log("Encountered an error in Db Connection")
-		} else {
-			console.log("Succesfully connected with DB");
-			clearInterval(timeOut)
-		}
-	})
-}, 5000);
+server.listen(port, () => console.log("Listening port " + port));
 
 
 // ++++++++++++++++ HTTP METHODS +++++++++++++++++++ //
@@ -52,6 +22,15 @@ let timeOut = setInterval(() => {
 app.get("/", (req, res) => {
 	res.send("E-skeleton-webrtc is up and running! :D")
 })
+
+
+app.get("/join-room", (req, res) => {
+	res.redirect(`/${uuidV4()}`)
+})
+
+app.get("/:room", (req, res) => {
+	res.send("loool")
+} )
 
 app.get("/users", async (req, res) => {					//	 B O R R A R
 	const users = await User.find();					//	 B O R R A R
